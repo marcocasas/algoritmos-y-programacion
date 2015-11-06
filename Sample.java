d30787b81dc08896f0d03966b3ef49743db325b2
marcocasas <marcoacasasm@gmail.com>
2015-11-05 21:17:10 -0600

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,9 +1,13 @@
+ArrayList <String> array = new ArrayList <String>();
+
 try {
   Scanner read = new Scanner(new File ("Data"));
   
   while (read.hasNext()) {
-    
+    array.add(read.nextInt());
   }
+  
+  read.close();
 } catch (Exception e) {
   System.out.print("Error.");
   System.exit(-1);
