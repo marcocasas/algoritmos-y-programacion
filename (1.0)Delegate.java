11d6d1b215e796f65b37eb45e35d7e972fa770ec
marcocasas <marcoacasasm@gmail.com>
2015-11-23 16:20:14 -0600

    Update (1.0)Delegate.java

--- (1.0)Delegate.java
+++ (1.0)Delegate.java
@@ -1,4 +1,42 @@
 public class Delegate implements Comparable <Delegate> {
-  private name;
+  private String name;
+  private boolean payment;
+  private String school;
+  private String country;
+  private int delegateNumber;
   
+  private static int delegateNumberMaster = 1;
+  
+  public Delegate() {
+    payment = false;
+    delegateNumber = delegateNumberMaster;
+    delegateNumberMaster++;
+  }
+  
+  public Delegate(String name, String school, String country) {
+    this();
+    this.name = name;
+    this.school = school.toUpperCase();
+    this.country = country.toUpperCase();
+  }
+  
+  public void registerPayment() {
+    payment = true;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public String getSchool() {
+    return school;
+  }
+  
+  public String country() {
+    return country;
+  }
+  
+  public int getDelegateNumber() {
+    return delegateNumber;
+  }
 }
