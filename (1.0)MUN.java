2f6162ea018e1fda9665adc00752aed838a91328
marcocasas <marcoacasasm@gmail.com>
2015-11-23 16:46:23 -0600

    Update (1.0)MUN.java

--- (1.0)MUN.java
+++ (1.0)MUN.java
@@ -1,9 +1,21 @@
 public class MUN {
   private String name;
   private String place;
+  private double cost;
   private LIO <Committee> committeeList;
   
   public MUN() {
     committeeList = new LIO <Committee> ();
+  }
+  
+  public MUN(String name, String place, String cost) {
+    this();
+    this.name = name;
+    this.place = place;
+    this.cost = cost;
+  }
+  
+  public double calculateProfits() {
+    
   }
 }
