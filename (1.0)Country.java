f781f8801231502607fef48902ca58e1d074cafb
marcocasas <marcoacasasm@gmail.com>
2015-11-23 17:03:18 -0600

    Create (1.0)Country.java

---
+++ (1.0)Country.java
@@ -0,0 +1,12 @@
+public class Country () {
+  private String name;
+  private boolean assigned;
+  
+  public Country() {
+  }
+  
+  public Country (String name) {
+    this.name = name;
+    assigned = false;
+  }  
+}
